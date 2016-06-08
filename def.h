5aac6ae7d1f4803d11263d327ad5708437e5ecc8
TheEris <seran@rocketmail.com>
2016-06-08 12:02:51 +0200

    Update def.h

--- def.h
+++ def.h
@@ -1,1 +1,8 @@
-#define stuff 666
+#define ERR 13 //Error line for timer
+#define DET 12 //Detonation line for timer (only for needy modules)
+
+#define c_RED 1
+#define c_BLUE 2
+#define c_YELLOW 3
+#define c_WHITE 4
+#define c_BLACK 5
