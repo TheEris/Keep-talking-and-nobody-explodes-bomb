297bcc3ec0f9bda8c7dceaf29b4b263e927f814a
TheEris <seran@rocketmail.com>
2016-06-09 16:29:59 +0200

    Update def.h

--- def.h
+++ def.h
@@ -1,6 +1,8 @@
-#define ERR 13 //Error line for timer
-#define DET 12 //Detonation line for timer (only for needy modules)
+#define ERR 13        //  Error line for timer
+#define DET 12        //  Detonation line for timer (only for needy modules)
 #define win_LED A0    //  green led on module
+
+#define moduleID 3    //  ID of the module
 
 #define c_RED 1
 #define c_BLUE 2
